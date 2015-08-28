43cc27056cc318b3f8f2844abbca1a38b89c1c8a
Dariusz Chojnacki <dariusz.chojnacki@gmail.com>
2015-08-28 16:09:16 +0200

    Adding simple loging.

--- src/main/java/pl/codepot/aggregatr/aggregation/ingredient/IngredientController.java
+++ src/main/java/pl/codepot/aggregatr/aggregation/ingredient/IngredientController.java
@@ -1,5 +1,7 @@
 package pl.codepot.aggregatr.aggregation.ingredient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,12 @@ import java.util.stream.Collectors;
         produces = MediaType.APPLICATION_JSON_VALUE)
 public class IngredientController {
 
+    private static final Logger log = LoggerFactory.getLogger(IngredientController.class);
+
     @RequestMapping(method = RequestMethod.POST)
     public Ingredients getIngredients(@RequestBody Order order) {
+        log.info("Recieving request...");
+
         return new Ingredients(order.getItems().stream().map(item->new Ingredient(item.name(), 200)).collect(Collectors.toList()));
     }
 
