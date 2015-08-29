819393a9ad335b98061fe3c6f79e3edcdf8c1f34
Dariusz Chojnacki <dariusz.chojnacki@gmail.com>
2015-08-29 08:39:17 +0200

    Correcting wiremock tests.

--- src/test/groovy/pl/codepot/aggregatr/aggregation/BaseMockMvcSpec.groovy
+++ src/test/groovy/pl/codepot/aggregatr/aggregation/BaseMockMvcSpec.groovy
@@ -1,12 +1,28 @@
 package pl.codepot.aggregatr.aggregation
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc
+import pl.codepot.aggregatr.aggregation.ingredient.Ingredient
 import pl.codepot.aggregatr.aggregation.ingredient.IngredientController
+import pl.codepot.aggregatr.aggregation.ingredient.IngredientService
+import pl.codepot.aggregatr.aggregation.ingredient.Ingredients
+import pl.codepot.aggregatr.aggregation.ingredient.Order
 import spock.lang.Specification
 
 abstract class BaseMockMvcSpec extends Specification {
 
+    protected static final int INGREDIENT_COUNT = 200
+
+    IngredientService ingredientService = Stub()
+
     def setup() {
-        RestAssuredMockMvc.standaloneSetup(new IngredientController())
+        setupMocks()
+
+        RestAssuredMockMvc.standaloneSetup(new IngredientController(ingredientService))
+    }
+
+    void setupMocks() {
+        ingredientService.sentIngredients(_) >> { Order order ->
+            return new Ingredients(order.items.collect { new Ingredient(it.name(), INGREDIENT_COUNT)})
+        }
     }
 
 }
